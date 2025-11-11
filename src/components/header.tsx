b2ff0599362bb01acbb9240831069d442d966e20
HeZc <hectoreduardozapata2911@gmail.com>
2025-11-11 01:40:16 -0500

    navbar done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,21 +5,34 @@ export default function Header() {
 
     return (
         <header className="bg-black" >
+
+            <nav
+                className="hidden md:block md:fixed md:left-0 md:top-0 md:h-screen md:w-[50vh] md:bg-black md:text-white md:text-lg md:flex md:flex-col
+                            md:items-center md:justify-center md:gap-8"
+            >
+                <img src="/Logo.png" alt="logo" className="w-24 h-24" />
+                <a className="hover:text-[gold] transition duration-300" href="home">Inicio</a>
+                <a className="hover:text-[gold] transition duration-300" href="services">Servicios</a>
+                <a className="hover:text-[gold] transition duration-300" href="about">Sobre mí</a>
+                <a className="hover:text-[gold] transition duration-300" href="contact">Contacto</a>
+            </nav>
+
             <button
                 className="md:hidden text-white p-4"
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
                 >
                 ☰
             </button>
-            <nav className="flex flex-col text-white">
-                <img src="/Logo.png" alt="logo" className="w-16 h-16" />
-                <a href="home">Inicio</a>
-                <a href="services">Servicios</a>
-                <a href="about">Sobre mí</a>
-                <a href="contact">Contacto</a>
+            {isMenuOpen && (
+            <nav
+                className="absolute top-16 left-0 w-full bg-black justify-center gap-4 text-white flex flex-col md:hidden"
+            >
+                <a className="hover:text-[gold] transition duration-300" href="home">Inicio</a>
+                <a className="hover:text-[gold] transition duration-300" href="services">Servicios</a>
+                <a className="hover:text-[gold] transition duration-300" href="about">Sobre mí</a>
+                <a className="hover:text-[gold] transition duration-300" href="contact">Contacto</a>
             </nav>
-
-            
+            )}
         </header>
     )
 }
